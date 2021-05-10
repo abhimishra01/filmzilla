2c7d9731ef3ee0eb4717fb06312626b8eabeb6dd
Moonwalker <abhisk.mhra@gmail.com>
2021-05-10 19:08:20 +0530

    more stylings in navbar and default comps

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import MovieCard from './comps/movieCard';
 import NavBar from './comps/navbar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <div className="home">
+      <MovieCard/>
+      </div>
       </div>
   );
 }
