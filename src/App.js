3f678ec8441a4b100b97c81c5a9de420df8873eb
Moonwalker <abhisk.mhra@gmail.com>
2021-05-10 18:12:11 +0530

    imported navbar and default stylings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import NavBar from './comps/navbar';
 
 function App() {
   return (
     <div className="App">
-      <h2>Lets Build FilmZilla App</h2>
-    </div>
+      <NavBar/>
+      </div>
   );
 }
 
