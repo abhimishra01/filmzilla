8c25f2ea9e222170f057065f5a250b72853e16d6
Moonwalker <abhisk.mhra@gmail.com>
2021-05-10 19:09:05 +0530

    moviecard test cmplt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import MovieCard from './comps/movieCard';
 import NavBar from './comps/navbar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <div className="home">
-      <MovieCard/>
-      </div>
       </div>
   );
 }
