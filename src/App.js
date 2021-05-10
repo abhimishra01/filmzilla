7ecac7bf2449245ed3fcb1c71ead8e1a8828fc73
Moonwalker <abhisk.mhra@gmail.com>
2021-05-10 22:07:02 +0530

    passed the method getMovies and the state movies as props to comps

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import MoviesPage from './comps/moviespage';
 import NavBar from './comps/navbar';
+import {useState,useEffect} from "react";
+import DEFAULT_API from "./api/api";
 
 function App() {
+// Declaring movies state array
+const [movies,setMovies] = useState([]);
+
+// Function to fetch movies from an api
+const getMovies = async(api) =>{
+    await fetch(api).then(res=>{
+        if(!res.ok){
+            alert("Something Went Wrong !");
+        }
+        return res.json();
+    }).then(data=>{
+        setMovies(data.results);
+        console.log(data.results);
+    }).catch(err=>alert(err.message));
+}
+
+useEffect(()=>{
+    getMovies(DEFAULT_API);
+},[])
+
+
   return (
     <div className="App">
-      <NavBar/>
-      <MoviesPage/>
+      <NavBar getMovies={getMovies}/>
+      <MoviesPage movies={movies}/>
       </div>
   );
 }
