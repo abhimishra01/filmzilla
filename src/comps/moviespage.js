67c1bcfde681cee6b389aea3b9079780c5a566c1
Moonwalker <abhisk.mhra@gmail.com>
2021-05-10 22:07:34 +0530

    shifted the method of getMovies to App.js file to provide global access of method to all comps

--- src/comps/moviespage.js
+++ src/comps/moviespage.js
@@ -1,30 +1,8 @@
 import MovieCard from "./movieCard";
-import {useState,useEffect} from "react";
 import "./moviespage.css";
-import DEFAULT_API from "../api/api";
 
-const MoviesPage = () => {
-
-// Declaring movies state array
-const [movies,setMovies] = useState([]);
-
-// Function to fetch movies from an api
-const getMovies = (api) =>{
-    fetch(api).then(res=>{
-        if(!res.ok){
-            alert("Something Went Wrong !");
-        }
-        return res.json();
-    }).then(data=>{
-        setMovies(data.results);
-        console.log(data.results);
-    }).catch(err=>alert(err.message));
-}
-
-useEffect(()=>{
-    getMovies(DEFAULT_API);
-},[])
-
+const MoviesPage = ({movies}) => {
+    
 return ( <div className="movies__page">
         <MovieCard  movies={movies}/>
     </div> );    
