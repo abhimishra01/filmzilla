6c12607d692b3e0d2364e224d79551a8284bd2e9
Moonwalker <abhisk.mhra@gmail.com>
2021-05-10 18:11:30 +0530

    added api endpoints for project

---
+++ src/api/api.js
@@ -0,0 +1,12 @@
+const DEFAULT_API = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d7eb245562d608898cbbab8c823d0259";
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=d7eb245562d608898cbbab8c823d0259&language=en-US&query=";
+
+const DEFAULT_POSTER_Path = "https://images.unsplash.com/photo-1598899134739-24c46f58b8c0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=738&q=80";
+
+const IMAGE_API = "https://image.tmdb.org/t/p/w1280";
+
+
+export default DEFAULT_API;
+
+export {SEARCH_API, DEFAULT_POSTER_Path, IMAGE_API};
