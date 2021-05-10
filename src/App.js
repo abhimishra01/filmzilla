cf2ce0f14d8de45c056e4c405a4d5ba45a58d53f
Moonwalker <abhisk.mhra@gmail.com>
2021-05-10 21:31:31 +0530

    imported moviespage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import MoviesPage from './comps/moviespage';
 import NavBar from './comps/navbar';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <MoviesPage/>
       </div>
   );
 }
