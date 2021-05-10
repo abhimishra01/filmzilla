4987710123a5c181bb9268dfb58d77e36727e6be
Moonwalker <abhisk.mhra@gmail.com>
2021-05-10 22:06:16 +0530

    props and methods for executing the search through the search API

--- src/comps/navbar.js
+++ src/comps/navbar.js
@@ -4,6 +4,10 @@ import "./navbar.css";
 const NavBar = () => {
     const [searchValue, setsearchValue] = useState("");
     
+    const handleSubmit =()=>{
+
+    }
+    const handleSearch =()=>{}
     const handleChange = (e) => {
         setsearchValue(e.target.value);
         console.log(searchValue);
@@ -14,10 +18,14 @@ const NavBar = () => {
         <h1>Welcome to FilmZilla</h1>
         <p>Your one stop to fetch your favourite movies . . .</p>
         <div className="nav__keys">
-            <button>User</button>
-            <input
+            <button
+            onClick={handleSearch}
+            >User</button>
+            <form onSubmit={handleSubmit}>
+             <input
             onChange={handleChange}
             type="search" placeholder="Search..."/>
+            </form>
         </div>
     </nav>
     </header>
