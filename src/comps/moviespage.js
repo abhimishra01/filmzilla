6133d7db927c194921f45d77917223b72c80cf77
Moonwalker <abhisk.mhra@gmail.com>
2021-05-11 14:53:17 +0530

    imported gridContainer to moviesPage.js

--- src/comps/moviespage.js
+++ src/comps/moviespage.js
@@ -1,14 +1,11 @@
+import GridContainer from "./gridContainer";
 import "./moviespage.css";
-import SingleLineGridList from "./gridListHorizontal";
+
 
 const MoviesPage = ({movies}) => {
     
 return ( <div className="movies__page">
-            <div className="heading__trending">
-                <h2>Trending Today</h2>
-      <SingleLineGridList movies={movies}/>
-            </div>
-
+       <GridContainer heading="Trending Today" contentArray={movies}/>
     </div> );    
 }
  
