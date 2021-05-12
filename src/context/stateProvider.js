3fb8fe31c11507aa7067cd5947e278739949ffeb
Moonwalker <abhisk.mhra@gmail.com>
2021-05-12 11:17:19 +0530

    added new piece of state inside context layer : Searching Movies or Tvshows

--- src/context/stateProvider.js
+++ src/context/stateProvider.js
@@ -6,10 +6,18 @@ export const StateContextProvider = (props) =>{
     const [user, setUser] = useState(null);
     const [movies, setMovies] = useState([]);
     const [tvShows, setTvShows] = useState([]);
+    const [searching_tvShows, setSearchingTVShows] = useState(false);
+    const [searching_movies, setSearchingMovies] = useState(false);
     return (
         // We can use the value of user, wherever we want in the components which are inside of the StateContext.Provider
         <StateContext.Provider 
-        value={{user:[user,setUser],movies_popular:[movies,setMovies],tvShows_popular:[tvShows,setTvShows]}}>
+        value={{
+            user:[user,setUser],
+            movies_popular:[movies,setMovies],
+            tvShows_popular:[tvShows,setTvShows],
+            searching_tvShows:[searching_tvShows, setSearchingTVShows],
+            searching_movies:[searching_movies, setSearchingMovies],
+        }}>
             {props.children}
         </StateContext.Provider>
     )
