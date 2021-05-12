55f351dc5d1bbc3e707ae7508d93a7ee022c2503
Moonwalker <abhisk.mhra@gmail.com>
2021-05-12 12:22:13 +0530

    firebaseConfig keys

---
+++ src/keys/firebaseConfig.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAlmKrBAy1l8B93t6MW3qUajBMZvip66cQ",
+    authDomain: "filmzilla-5dee3.firebaseapp.com",
+    projectId: "filmzilla-5dee3",
+    storageBucket: "filmzilla-5dee3.appspot.com",
+    messagingSenderId: "866195778907",
+    appId: "1:866195778907:web:68f1ccb695c3fb604dfe7e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
