edf3c3f9a9cdcb2197a98866bc6d158f42a5ec4d
Moonwalker <abhisk.mhra@gmail.com>
2021-05-12 12:10:56 +0530

    feat : Search On clear method back to home &searching state implementation:

--- src/comps/moviespage.js
+++ src/comps/moviespage.js
@@ -3,29 +3,37 @@ import MovieCard from "./movieCard";
 import "./moviespage.css";
 import {StateContext} from "../context/stateProvider"; 
 import {useContext} from "react";
+import { CircularProgress } from "@material-ui/core";
 
 const MoviesPage = () => {
 
 const [searchingTvResult] = useContext(StateContext).searching_tvShows_result[0];
 const [searchingMovieResult] = useContext(StateContext).searching_movies_result[0];
+const [searching, setSearching] = useContext(StateContext).searching;
 
-if(searchingTvResult){
-    return (<div className="movies__page">
+if(searching) {
+    if(searchingTvResult){
+        return (<div className="movies__page">
         <MovieCard contentArray={searchingTvResult} tvShow={true}/>
     </div> )
 }
-
 if(searchingMovieResult){
     return (<div className="movies__page">
         <MovieCard contentArray={searchingMovieResult}/>
     </div> )
 }
+}
 
-else{
+if(!searching){
     return ( <div className="movies__page">
         <GridContainer heading="Trending Today : Movies"  movieContainer={true} />
         <GridContainer heading="Trending Today : TV Shows" tvShowContainer={true} />
         </div> );    
 } 
+else {
+    setSearching(null);
+    return <CircularProgress/>
+}
+
 }
 export default MoviesPage;  
--- src/comps/navbar.js
+++ src/comps/navbar.js
@@ -4,17 +4,34 @@ import {SEARCH_API} from "../api/api";
 import {StateContext} from "../context/stateProvider";
 
 const NavBar = ({fetchData}) => {
+
+    // Fetching pieces of search states from context layer
     const [searching_movie_result, setSearchingMoviesResult] = useContext(StateContext).searching_movies_result
+const [searching, setSearching] = useContext(StateContext).searching;
+    
     console.log(searching_movie_result);
+    console.log(searching);
+    
     const [searchValue, setsearchValue] = useState("");
-    const handleSubmit =(e)=>{
+
+    // SearchBox Submit Handler
+    const handleSubmit = async(e)=>{
         e.preventDefault();
-        fetchData(SEARCH_API + searchValue,setSearchingMoviesResult);
+        setSearching(true);
+        await fetchData(SEARCH_API + searchValue,setSearchingMoviesResult);
     }
-    const handleSearch =()=>{}
+
+
+    // Searchbox clear Handler
+ 
+    
+    
+    // Search field Change handler
     const handleChange = (e) => {
+        if(e.target.value === ''){
+            setSearching(null);
+        }       
         setsearchValue(e.target.value);
-        console.log(searchValue);
     }
     return ( 
     <header>
@@ -23,7 +40,7 @@ const NavBar = ({fetchData}) => {
         <p>Your one stop to your favourite Entertainments . . .</p>
         <div className="nav__keys">
             <button
-            onClick={handleSearch}
+            // onClick={handleSearch}
             >User</button>
             <form onSubmit={handleSubmit}>
              <input
