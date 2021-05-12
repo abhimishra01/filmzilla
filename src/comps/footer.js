0d3f131ae3a5033deff398b104108b23c5e149bd
Moonwalker <abhisk.mhra@gmail.com>
2021-05-12 16:18:58 +0530

    Footer section for the webapp

---
+++ src/comps/footer.js
@@ -0,0 +1,14 @@
+import "./footer.css"
+
+const FooterBar = () => {
+    return ( <footer>
+        <h3 class='copyright'> @Abhishek Mishra 2021 <span>All rights reserved</span></h3>
+        <div className="footer_link">
+            {/* <a href="https://www.linkedin.com/in/moonwalkerabhi/">
+                Contact the developer
+            </a> */}
+        </div>
+    </footer> );
+}
+ 
+export default FooterBar;
