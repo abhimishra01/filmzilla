49a2b6dfd3042d9c07f542712e6f41b23ac346cb
Moonwalker <abhisk.mhra@gmail.com>
2021-05-12 14:43:20 +0530

    styling for togglebtns

--- src/comps/toggleBtn.js
+++ src/comps/toggleBtn.js
@@ -1,6 +1,8 @@
 import ToggleButton from "@material-ui/lab/ToggleButton";
 import ToggleButtonGroup from "@material-ui/lab/ToggleButtonGroup";
 import {useState} from "react";
+import "./togglebtn.css";
+
 export default function TrendingToggleButton() {
   const [alignment, setAlignment] = useState("left");
 
@@ -10,17 +12,20 @@ export default function TrendingToggleButton() {
 
   return (
     <ToggleButtonGroup
-      value={alignment}
-      exclusive
-      onChange={handleAlignment}
+    value={alignment}
+    exclusive
+    onChange={handleAlignment}
       aria-label="text alignment"
-    >
-      <ToggleButton value="left" aria-label="left aligned">
-        {/* <FormatAlignLeftIcon /> */}
+      >
+      <ToggleButton
+      style={{color:"var(--secondary)",fontSize:"0.8rem",padding:"0.98rem", fontFamily:"Rubik, sans-serif"}}  
+      value="left" aria-label="left aligned">
         Today
       </ToggleButton>
-      <ToggleButton value="center" aria-label="centered">
-        {/* <FormatAlignCenterIcon /> */}
+      <ToggleButton 
+        style={{color:"var(--secondary)",fontSize:"0.8rem",padding:"0.98rem", fontFamily:"Rubik, sans-serif"}}  
+      value="center" 
+      aria-label="centered">
         This Week
       </ToggleButton>
     </ToggleButtonGroup>
