32d2fee97f7231c79174e2f4369442f849ffbaf1
Moonwalker <abhisk.mhra@gmail.com>
2021-05-12 11:28:42 +0530

    search Result output conditons in moviesPage.js

--- src/comps/movieCard.js
+++ src/comps/movieCard.js
@@ -1,24 +1,24 @@
 import "./movieCard.css"
 import {DEFAULT_POSTER_Path,IMAGE_API} from "../api/api";
 
-const MovieCard = ({movies}) => {
+const MovieCard = ({contentArray, tvShow}) => {
     
     return (
         <>
         {/* Poster of the movie */}
-        {movies && movies.map(movie =>{
+        {contentArray && contentArray.map(tile =>{
             return (
-                <div key={movie.id} className="movie__card">
+                <div key={tile.id} className="movie__card">
         {/* If movie's poster path is available. show, otheriwse show a default poster img */}
-        <img src={movie.poster_path?IMAGE_API + movie.poster_path : DEFAULT_POSTER_Path} alt="" />
+        <img src={tile.poster_path?IMAGE_API + tile.poster_path : DEFAULT_POSTER_Path} alt="" />
         {/* Movie Card Header : Movie name & Rating */}
         <div className="movie__cardHeader">
-            <h3>{movie.title}</h3>
-            <p>{movie.vote_average}</p>
+            <h3>{tvShow ? tile.name : tile.title}</h3>
+            {/* <p>{movie.vote_average}</p> */}
         </div>
         {/* Movie description : by default is hidden */}
         <div className="movie__cardDesc">
-            <p>{movie.overview}</p>
+            <p>{tile.overview}</p>
         </div>
                 </div>
             )
--- src/comps/moviespage.js
+++ src/comps/moviespage.js
@@ -1,7 +1,26 @@
 import GridContainer from "./gridContainer";
+import MovieCard from "./movieCard";
 import "./moviespage.css";
+import {StateContext} from "../context/stateProvider"; 
+import {useContext} from "react";
+
 const MoviesPage = () => {
-    
+
+const [searchingTvResult] = useContext(StateContext).searching_tvShows_result[0];
+const [searchingMovieResult] = useContext(StateContext).searching_movies_result[0];
+
+if(searchingTvResult){
+    return (<div className="movies__page">
+        <MovieCard contentArray={searchingTvResult} tvShow={true}/>
+    </div> )
+}
+
+if(searchingMovieResult){
+    return (<div className="movies__page">
+        <MovieCard contentArray={searchingMovieResult}/>
+    </div> )
+}
+
 return ( <div className="movies__page">
        <GridContainer heading="Trending Today : Movies"  movieContainer={true} />
        <GridContainer heading="Trending Today : TV Shows" tvShowContainer={true} />
