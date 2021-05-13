34c9749675dd6439c023c2626afa06b25ef8af4b
Moonwalker <abhisk.mhra@gmail.com>
2021-05-13 22:02:39 +0530

    no Bookmarks page Styling added

--- src/comps/bookmarks.js
+++ src/comps/bookmarks.js
@@ -1,8 +1,9 @@
+import "./bookmarks.css";
+import {Button} from "@material-ui/core";
 import { CircularProgress } from "@material-ui/core";
 import useFireStore from "../hooks/useFireStore";
 import MovieCard from "./movieCard";
 import {useParams} from "react-router-dom";
-
 const Bookmarks = () => {
 
     const collectionName = useParams().content;
@@ -18,11 +19,12 @@ const Bookmarks = () => {
 
     if(inProgress === false && contentArray.length === 0){
         return ( <div className="no__bookmarks">
-            <h2>No Bookmarks yet !</h2>
+            <h2>No Bookmarks yet ! <Button className="homeBtn">Back To Home</Button></h2>
              </div>)
     }
     return ( <div className="movies__page">
-        {inProgress &&  <CircularProgress className="circular__progress"/> }
+        {inProgress &&  <div className="loading"><CircularProgress className="circular__progress"/> 
+        </div> }
       {contentArray.length > 0 &&  <MovieCard contentArray={contentArray} tvShow={checkTvShow} bookmark={true}/> }
     </div>
     );
