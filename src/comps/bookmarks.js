fab944cfe288ecda07107d59c1c085c32352d098
Moonwalker <abhisk.mhra@gmail.com>
2021-05-13 22:04:49 +0530

    back to home push added to no bookmarks page

--- src/comps/bookmarks.js
+++ src/comps/bookmarks.js
@@ -3,9 +3,11 @@ import {Button} from "@material-ui/core";
 import { CircularProgress } from "@material-ui/core";
 import useFireStore from "../hooks/useFireStore";
 import MovieCard from "./movieCard";
-import {useParams} from "react-router-dom";
+import {useParams,useHistory} from "react-router-dom";
+
 const Bookmarks = () => {
 
+    const history = useHistory();
     const collectionName = useParams().content;
     const {inProgress,contentArray} = useFireStore(collectionName);
     const checkTvShow = () =>{
@@ -17,11 +19,16 @@ const Bookmarks = () => {
         }
     }
 
+    const backToHome = () => {
+        history.push("/");
+    }
+
     if(inProgress === false && contentArray.length === 0){
         return ( <div className="no__bookmarks">
-            <h2>No Bookmarks yet ! <Button className="homeBtn">Back To Home</Button></h2>
+            <h2>No Bookmarks yet ! <Button  onClick={backToHome} className="homeBtn">Back To Home</Button></h2>
              </div>)
     }
+
     return ( <div className="movies__page">
         {inProgress &&  <div className="loading"><CircularProgress className="circular__progress"/> 
         </div> }
