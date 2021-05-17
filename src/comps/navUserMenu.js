1a69848a423b09444b9a5edee36222027e573156
Moonwalker <abhisk.mhra@gmail.com>
2021-05-17 22:39:10 +0530

    navbar responsiveness updated

--- src/comps/navUserMenu.js
+++ src/comps/navUserMenu.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import ClickAwayListener from '@material-ui/core/ClickAwayListener';
-import Grow from '@material-ui/core/Grow';
-import Paper from '@material-ui/core/Paper';
-import Popper from '@material-ui/core/Popper';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import { makeStyles } from '@material-ui/core/styles';
-import {StateContext} from "../context/stateProvider";
-import {auth} from "../keys/firebaseConfig";
-import {useHistory} from "react-router-dom";
+import React from "react";
+import Button from "@material-ui/core/Button";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { makeStyles } from "@material-ui/core/styles";
+import { StateContext } from "../context/stateProvider";
+import { auth } from "../keys/firebaseConfig";
+import { useHistory } from "react-router-dom";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-},
-paper: {
+    display: "flex",
+  },
+  paper: {
     marginRight: theme.spacing(2),
   },
 }));
 
 export default function NavUserMenu() {
-
-// Fetching the user state from our context API layer :
-const [user,setUser] = React.useContext(StateContext).user;
-
+  // Fetching the user state from our context API layer :
+  const [user, setUser] = React.useContext(StateContext).user;
 
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
@@ -43,21 +42,21 @@ const [user,setUser] = React.useContext(StateContext).user;
   };
 
   const history = useHistory();
-  const handleHome = () =>{
+  const handleHome = () => {
     history.push("/");
-  }
+  };
 
-  const handleLogout = async() => {
-    try{
-      await auth.signOut()
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
       setUser(null);
-    } catch(err){
-       alert(err.message);
-     } 
-  }
+    } catch (err) {
+      alert(err.message);
+    }
+  };
 
   function handleListKeyDown(event) {
-    if (event.key === 'Tab') {
+    if (event.key === "Tab") {
       event.preventDefault();
       setOpen(false);
     }
@@ -75,31 +74,48 @@ const [user,setUser] = React.useContext(StateContext).user;
 
   return (
     <div className={classes.root}>
-     
       <div>
         <Button
           ref={anchorRef}
-          aria-controls={open ? 'menu-list-grow' : undefined}
+          aria-controls={open ? "menu-list-grow" : undefined}
           aria-haspopup="true"
           onClick={handleToggle}
-          >
-          <img 
-          style={{
-            objectFit:"contain",height:"8vh",borderRadius:"80%"
-        }}
-          src={user.photoURL} alt="" />
+        >
+          <img
+            style={{
+              objectFit: "contain",
+              height: "8vh",
+              borderRadius: "80%",
+            }}
+            src={user.photoURL}
+            alt=""
+          />
+          <ExpandMoreIcon color="primary" fontSize="large" />
         </Button>
-        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+        <Popper
+          open={open}
+          anchorEl={anchorRef.current}
+          role={undefined}
+          transition
+          disablePortal
+        >
           {({ TransitionProps, placement }) => (
             <Grow
               {...TransitionProps}
-              style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+              style={{
+                transformOrigin:
+                  placement === "bottom" ? "center top" : "center bottom",
+              }}
             >
               <Paper>
                 <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                  <MenuList
+                    autoFocusItem={open}
+                    id="menu-list-grow"
+                    onKeyDown={handleListKeyDown}
+                  >
                     <MenuItem onClick={handleHome}>Home</MenuItem>
-                    <MenuItem onClick={handleLogout} >Logout</MenuItem>
+                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
                   </MenuList>
                 </ClickAwayListener>
               </Paper>
--- src/comps/navbar.js
+++ src/comps/navbar.js
@@ -1,57 +1,57 @@
-import {useContext,useState} from "react";
+import { useContext, useState } from "react";
 import "./navbar.css";
-import {SEARCH_API} from "../api/api";
-import {StateContext} from "../context/stateProvider";
+import { SEARCH_API } from "../api/api";
+import { StateContext } from "../context/stateProvider";
 import NavUserMenu from "./navUserMenu";
-const NavBar = ({fetchData}) => {
 
-    // Fetching pieces of search states from context layer
-    const [searching_movie_result, setSearchingMoviesResult] = useContext(StateContext).searching_movies_result
-const [searching, setSearching] = useContext(StateContext).searching;
-    
-    console.log(searching_movie_result);
-    console.log(searching);
-    
-    const [searchValue, setsearchValue] = useState("");
+const NavBar = ({ fetchData }) => {
+  // Fetching pieces of search states from context layer
+  const [searching_movie_result, setSearchingMoviesResult] =
+    useContext(StateContext).searching_movies_result;
+  const [searching, setSearching] = useContext(StateContext).searching;
 
-    // SearchBox Submit Handler
-    const handleSubmit = async(e)=>{
-        e.preventDefault();
-        setSearching(true);
-        await fetchData(SEARCH_API + searchValue,setSearchingMoviesResult);
+  console.log(searching_movie_result);
+  console.log(searching);
+
+  const [searchValue, setsearchValue] = useState("");
+
+  // SearchBox Submit Handler
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setSearching(true);
+    await fetchData(SEARCH_API + searchValue, setSearchingMoviesResult);
+  };
+
+  // Searchbox clear Handler
+
+  // Search field Change handler
+  const handleChange = (e) => {
+    if (e.target.value === "") {
+      setSearching(null);
     }
-
-
-    // Searchbox clear Handler
- 
-    
-    
-    // Search field Change handler
-    const handleChange = (e) => {
-        if(e.target.value === ''){
-            setSearching(null);
-        }       
-        setsearchValue(e.target.value);
-    }
-    return ( 
+    setsearchValue(e.target.value);
+  };
+  return (
     <header>
-    <nav>
+      <nav>
         <h1>Welcome to FilmZilla</h1>
         <p>Your one stop to your favourite Entertainments . . .</p>
         <div className="nav__keys">
-                <div className="nav__userBtn">
-                    <NavUserMenu/>
-                </div>
-            {/* <NavUserBtn/> */}
-            <form onSubmit={handleSubmit}>
-             <input
-            onChange={handleChange}
-            type="search" placeholder="Search Movies or Shows.."/>
-            </form>
+          <div className="nav__userBtn">
+            <NavUserMenu />
+          </div>
+          {/* <NavUserBtn/> */}
+          <form onSubmit={handleSubmit}>
+            <input
+              onChange={handleChange}
+              type="search"
+              placeholder="Search Movies or Shows.."
+            />
+          </form>
         </div>
-    </nav>
+      </nav>
     </header>
-     );
-    }
-    
-    export default NavBar;
+  );
+};
+
+export default NavBar;
